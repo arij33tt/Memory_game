7280cc41578327faab2ad9bd72edb5d29fddf203
Arijeet Aman <arijeetaman@gmail.com>
2022-10-24 01:43:21 +0530

    Memory_Game

---
+++ index.js
@@ -0,0 +1,117 @@
+const cardArray = [
+{
+  name:"fries",
+  img:'./images/fries.png',
+},
+{
+  name:"cheeseburger",
+  img:'./images/cheeseburger.png',
+},
+{
+  name:"hotdog",
+  img:'./images/hotdog.png',
+
+},
+{
+  name:"ice-cream",
+  img:'./images/ice-cream.png',
+},
+{
+  name:"milkshake",
+  img:'./images/milkshake.png',
+},
+{
+  name:"pizza",
+  img:'./images/pizza.png',
+},
+
+// copies it because we need 2 of each card
+  
+{
+  name:"fries",
+  img:'./images/fries.png',
+},
+{
+  name:"cheeseburger",
+  img:'./images/cheeseburger.png',
+},
+{
+  name:"hotdog",
+  img:'./images/hotdog.png',
+
+},
+{
+  name:"ice-cream",
+  img:'./images/ice-cream.png',
+},
+{
+  name:"milkshake",
+  img:'./images/milkshake.png',
+},
+{
+  name:"pizza",
+  img:'./images/pizza.png',
+}
+]
+
+// we want to shuffle the cards
+//so here we use the random method of javascript to shuffle the cards randomly
+
+cardArray.sort(()=>0.5 -Math.random());
+
+// compare the array elements ,  that they are smaller or larger than 0.5 or not  and randomly shuffle it
+
+
+
+
+ const gridDisplay= document.querySelector('#grid');
+
+ const cardsChosen = [];
+
+ function createBorad (){
+
+  // for 12 images we need 12 cards to show 
+  // so we use a for loop to create 12 cards
+  //because we have 12 images in the array
+  //basically we are creating 12 cards and adding the images to it
+
+  
+   for(let i =0 ; i<cardArray.length;i++){
+
+     const card= document.createElement('img');
+
+  card.setAttribute('src','./images/blank.png');
+
+  card.setAttribute('data-id',i);
+
+  card.addEventListener('click',flipCard);
+
+  gridDisplay.appendChild(card)
+
+  // console.log(card , i);
+
+   }
+ }
+
+ 
+ createBorad();
+
+ function checkMatch(){
+
+ }
+
+ function flipCard(){
+
+    let cardId= this.getAttribute('data-id');
+
+    cardsChosen.push(cardArray[cardId].name);
+
+    this.setAttribute('src',cardArray[cardId].img);
+
+    if(cardsChosen.length===2){
+      
+      settimeout(checkMatch,500);
+    }
+
+  
+ }
