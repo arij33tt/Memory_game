802c150a66de4b8899dbc47939d66bf615aa4b1d
Arijeet Aman <arijeetaman@gmail.com>
2022-10-25 22:07:59 +0530

    final js code

--- index.js
+++ index.js
@@ -66,14 +66,18 @@ cardArray.sort(()=>0.5 -Math.random());
  // we want an array to store the cards that are chosen
  // so we create a variable cardsChosen
  // and we want to store the name of the card that is chosen
- const cardsChosen = [];
+ let cardsChosen = [];
+ let cardsChosenIds=[];
+ const cardsWon = [];
+ const resultDisplay = document.querySelector('#result');
+
 
  function createBorad (){
 
-                               // for 12 images we need 12 cards to show 
-                               // so we use a for loop to create 12 cards
-                               //because we have 12 images in the array
-                               //basically we are creating 12 cards and adding the images to it
+                       // for 12 images we need 12 cards to show 
+                       // so we use a for loop to create 12 cards
+                       //because we have 12 images in the array
+                       //basically we are creating 12 cards and adding the images to it
 
   
    for(let i =0 ; i<cardArray.length;i++){
@@ -94,19 +98,43 @@ cardArray.sort(()=>0.5 -Math.random());
  
  createBorad();
 
+ // this function is used to check the match of the cards 
+
  function checkMatch(){
 
-  document.querySelectorAll('img')
+  const cards = document.querySelectorAll('img')
+
+    const optionOneId = cardsChosenIds[0];
+    const optionTwoId = cardsChosenIds[1];
+
 
     console.log("checking for match");
     
-if(cardsChosen[0]=== cardsChosen[1]){
+if(cardsChosen[0]== cardsChosen[1]){
 
-  alert("you found a match");
-}
+  cards[optionOneId].setAttribute('src','./images/white.png')
+  cards[optionTwoId].setAttribute('src','./images/white.png');
 
-
+  cards[optionOneId].removeEventListener('click',flipCard);
+  cards[optionTwoId].removeEventListener('click',flipCard);
+  cardsWon.push(cardsChosen);
+  
  }
+else{
+  cards[optionOneId].setAttribute('src','./images/blank.png');
+  cards[optionTwoId].setAttribute('src','./images/blank.png');
+}
+resultDisplay.innerHTML= cardsWon.length;
+ cardsChosen=[];
+ cardsChosenIds=[];
+
+if(cardsWon.length === 6){
+  // alert("hip hip hurray you won");
+  resultDisplay.innerHTML="Congratulations you found them all ";
+}
+ }
+
+// the function below is used to flip the cards 
 
  function flipCard(){
 
@@ -115,6 +143,8 @@ if(cardsChosen[0]=== cardsChosen[1]){
     cardsChosen.push(cardArray[cardId].name);
 
     this.setAttribute('src',cardArray[cardId].img);
+
+cardsChosenIds.push(cardId)
 
     if(cardsChosen.length===2){
 
